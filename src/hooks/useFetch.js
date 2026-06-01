9d320853fae33586efe5c8d0c130562746a0339e
Soumya <mishrasoumya727@gmail.com>
2026-06-01 15:15:12 +0530

    Update useFetch.js

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -3,41 +3,41 @@ import { useState, useEffect } from "react";
 
 function useFetch(url) {
 
-  // Store fetched data
-  const [data, setData] = useState([]);
+    // Stores fetched data
+    const [data, setData] = useState([]);
 
-  // Track loading status
-  const [loading, setLoading] = useState(true);
+    //It is used to Track loading status
+    const [loading, setLoading] = useState(true);
 
-  // Store error messages
-  const [error, setError] = useState("");
+    //It is used to Store error messages
+    const [error, setError] = useState("");
 
-  useEffect(() => {
+    useEffect(() => {
 
-    // Function to fetch data from API
-    async function fetchData() {
-      try {
-        const response = await fetch(url);
+        // Function to fetch data from API
+        async function fetchData() {
+            try {
+                const response = await fetch(url);
 
-        if (!response.ok) {
-          throw new Error("Failed to fetch data");
+                if (!response.ok) {
+                    throw new Error("Failed to fetch data");
+                }
+
+                const result = await response.json();
+
+                setData(result);
+            } catch (err) {
+                setError(err.message);
+            } finally {
+                setLoading(false);
+            }
         }
 
-        const result = await response.json();
+        fetchData();
 
-        setData(result);
-      } catch (err) {
-        setError(err.message);
-      } finally {
-        setLoading(false);
-      }
-    }
+    }, [url]); //This Runs when URL changes
 
-    fetchData();
-
-  }, [url]); // Runs when URL changes
-
-  return { data, loading, error };
+    return { data, loading, error };
 }
 
 export default useFetch;
